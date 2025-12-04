8679bff50120ed2395ea6b63490be2c8346dd4df
Kent clarence Evangelista <kentclarence5368@gmail.com>
2025-12-04 23:32:49 +0800

    adding config example

---
+++ app/config/config.example.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'reservation');//put here the database name///
+define('DB_USER', 'root');
+define('DB_PASS', ''); // Put your real password here
+?>
